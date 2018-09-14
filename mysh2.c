1ec221ab83053cde1fec981e047303ae5685ebff
Bas <bascasper@gmail.com>
2018-09-14 23:47:46 +0200

    ls and cd support

--- mysh2.c
+++ mysh2.c
@@ -6,27 +6,30 @@
 #include <stdlib.h>
 
 int main(int argc, char const *argv[]) {
-  char prog[100];
-  char *arg[10], *p;
   int i = 0;
-  char *env_args[] = {"PATH=/bin", (char*)0};
+  char prog[1024];
+  char *arg[10]={}, *p;
 
   while (1) {
     fprintf(stdout, "$ ");
-    if (fgets( prog, sizeof(prog), stdin) != NULL) {
+    if (fgets(prog, sizeof(prog), stdin) != NULL) {
+      i = 0;
       prog[strcspn(prog, "\n")] = 0;
       p = strtok(prog, " ");
       while (p != NULL) {
-        arg[i++] = prog;
+        arg[i++] = p;
         p = strtok(NULL, " ");
       }
-      arg[i+1] = "0";
+      arg[9] = '\0';
     }
+
     if (strcmp(prog,"exit")==0) {
       exit(1);
+    } else if (strcmp(prog, "cd")==0) {
+      chdir(arg[1]);
     } else {
       if (fork()==0) {
-        execle(arg[0], arg, env_args);
+        execvp(arg[0], arg);
         perror("Error calling exec()! \n");
         exit(1);
       } else {
