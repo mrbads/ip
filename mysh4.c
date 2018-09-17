2c544777310992f5dedaa87443634d6aafc66f8b
Bas <bascasper@gmail.com>
2018-09-18 00:32:00 +0200

    looped pipe + finished

--- mysh4.c
+++ mysh4.c
@@ -5,93 +5,93 @@
 #include <sys/wait.h>
 #include <stdlib.h>
 
+void run(char *prog, int rd, int wr) {
+  char *p, *arg[10]={};
+  int i = 0;
+
+  p = strtok(prog, " ");
+  if (p != NULL) {
+    // printf("%s\n", p);
+    while (p != NULL) {
+      arg[i++] = p;
+      p = strtok(NULL, " ");
+    }
+  }
+  if (fork() == 0) {
+    if (rd) {
+      dup2(rd, 0);
+    }
+    if (wr) {
+      dup2(wr, 1);
+    }
+    execvp(arg[0], arg);
+  } else {
+    if (rd) {
+      close(rd);
+    }
+    if (wr) {
+      close(wr);
+    }
+  }
+}
+
 int main(int argc, char const *argv[]) {
   int x = 0, count = 0;
   char prog[100];
-  char *arg[][10]={}, *p;
+  char *arg[10]={}, *p, *dest;
   int (*pfd)[2];
-  char buf[100];
 
   while (1) {
     fprintf(stdout, "$ ");
     if (fgets(prog, sizeof(prog), stdin) != NULL) {
-      p = prog;
+      // printf("%s\n", prog);
       prog[strcspn(prog, "\n")] = 0;
-      x = 0;
-      arg[1][1] = ".";
-      arg[2][0] = '\0';
-      while ((p = strchr(p,'|')) != NULL) {
-        count++;
-        p++;
-      }
-      if (count != 0) {
-        for (size_t i = 0; i < count; i++) {
-          p = strtok(prog, "|");
-          if (p != NULL) {
-            p = strtok(p, " ");
-            while (p != NULL) {
-              arg[i][x++] = p;
-              p = strtok(NULL, " ");
-            }
-            arg[i][9] = '\0';
-          }
-        }
+      p = prog;
+    }
+    while ((p = strchr(p, '|')) != NULL) {
+      count++;
+      p++;
+    }
+
+    for (size_t i = 0; i < count+1; i++) {
+      if (i == 0) {
+        p = strtok(prog, "|");
+        // printf("%s\n", p);
+        arg[i] = p;
       } else {
-        p = strtok(prog, " ");
-        if (p != NULL) {
-          p = strtok(p, " ");
-          while (p != NULL) {
-            arg[0][x++] = p;
-            p = strtok(NULL, " ");
-          }
-          arg[0][9] = '\0';
-        }
+        p = strtok(NULL, "|");
+        // printf("%s\n", p);
+        arg[i] = p;
       }
     }
-    printf("%s\n", prog);
-    printf("%s\n", arg[0][1]);
 
-    if (strcmp(prog, "exit")==0) {
+    if (strcmp(arg[0], "exit") == 0) {
       exit(1);
-    } else if (strcmp(prog, "cd")==0) {
-      printf("%s\n", arg[0][1]);
-      chdir(arg[0][1]);
-      getcwd(buf, sizeof(buf));
-      printf("%s\n", buf);
+    } else if (strncmp(arg[0], "cd", 2) == 0) {
+      dest = strtok(arg[0], " ");
+      dest = strtok(NULL, " ");
+      chdir(dest);
     } else {
-      for (size_t i = 0; i < count-1; i++) {
-        pipe(pfd[i]);
+      if ((pfd = malloc(sizeof(*pfd) * count)) == NULL) {
+        perror("malloc");
+        exit(EXIT_FAILURE);
       }
-      for (size_t i = 0; i < count; i++) {
-        if (i == 0) {
-          if (fork()==0) {
-            printf("First fork\n");
-            if (arg[2][0] != NULL) {
-              dup2(*pfd[1], 1);
-              close(*pfd[1]);
-            }
-            execvp(arg[1][0], arg[1]);
-            perror("Error calling exec()! \n");
-            exit(1);
-          }
-        } else if (i < count-1) {
-          if (fork()==0) {
-            dup2(*pfd[i-1], 0);
-            dup2(*pfd[i+1], 1);
-            close(*pfd[i-1]);
-            close(*pfd[i+1]);
-          }
-        } else {
-          if (fork()==0) {
-            dup2(*pfd[i-1], 0);
-            close(*pfd[i-1]);
-          }
+
+      for (int i = 0; i < count; i++) {
+        if (pipe(pfd[i]) < 0) {
+          perror("pipe");
+          exit(EXIT_FAILURE);
         }
       }
-      for (size_t i = 0; i < count; i++) {
-        close(*pfd[i]);
+      for (size_t i = 0; i < count+1; i++) {
+        if (i == 0) {
+          run(arg[i], 0, pfd[i][1]);
+        } else if (i < count) {
+          run(arg[i], pfd[i-1][0], pfd[i][1]);
+        } else {
+          run(arg[i], pfd[i-1][0], 0);
+        }
       }
-      wait(NULL);
     }
     sleep(1);
   }
