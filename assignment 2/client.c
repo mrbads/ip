dca90a8be3f83399704d1d5727ece6a0f62f70a0
Bas van der Borden <bascasper@gmail.com>
2018-09-26 00:08:45 +0200

    it 1

--- assignment 2/client.c
+++ assignment 2/client.c
@@ -3,6 +3,10 @@
 #include <stdio.h>
 #include <sys/types.h>
 #include <sys/socket.h>
+#include <netdb.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
 
 int GET = 103;
 int PUT = 112;
@@ -11,10 +15,46 @@ int NOTFOUND = 110;
 
 int connect(int sockfd, const struct sockaddr *serv_addr, socklen_t addrlen);
 
-int main(int argc, char const *argv[]) {
+int main(int argc, char *argv[]) {
   char const *hostname = argv[1], *port = argv[2];
+  struct addrinfo hints, *result, *p;
+  int fd, status;
+  char msg[256];
 
+  printf("%i\n", argc);
   printf("%s\n", hostname);
   printf("%s\n", port);
+  if (strcmp(argv[4],"put") == 0) {
+    msg = PUT + argv[5] + argv[6];
+  } else if (strcmp(argv[4],"get") == 0) {
+    msg = GET;
+  }
+  printf("%s\n", msg);
+
+  memset(&hints, 0, sizeof(hints));
+  hints.ai_family = AF_UNSPEC;
+  hints.ai_socktype = SOCK_STREAM;
+
+  status = getaddrinfo(hostname, port, &hints, &result);
+  if (status != 0) {
+    fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(status));
+    exit(EXIT_FAILURE);
+  }
+
+  for (p = result; p != NULL; p = p->ai_next) {
+    fd = socket(p->ai_family, p->ai_socktype, 0);
+    if (fd == -1) {
+      fprintf(stderr, "socket error\n");
+      exit(EXIT_FAILURE);
+    }
+    if (connect(fd, p->ai_addr, p->ai_addrlen) == -1) {
+      fprintf(stderr, "connect error\n");
+      exit(EXIT_FAILURE);
+    }
+
+  }
+
+
+  freeaddrinfo(result);
   return 0;
 }
--- assignment 2/serv1.c
+++ assignment 2/serv1.c
@@ -1,15 +1,55 @@
 // Iterative server
 
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netdb.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+
 int GET = 103;
 int PUT = 112;
 int FOUND = 102;
 int NOTFOUND = 110;
+int BACKLOG = 5;
 
-#include <sys/socket.h>
 
 int main(int argc, char const *argv[]) {
-  int listen(int sockfd, int backlog=5);
-  int fd, newfd;
+  int fd, newsock, res;
+  struct sockaddr_in addr, client_addr;
+  socklen_t addrlen;
+  char buf[100];
+  socklen_t fromlen = sizeof(client_addr);
 
+  fd = socket(AF_INET, SOCK_STREAM, 0);
+  addrlen = sizeof(struct sockaddr_in);
+  memset(&addr, 0, addrlen);
+  addr.sin_family = AF_INET;
+  addr.sin_port = htons(1235);
+  addr.sin_addr.s_addr = htonl(INADDR_ANY);
+
+  if (bind(fd, (struct sockaddr *) &addr, addrlen) == -1) {
+    fprintf(stderr, "bind error");
+    exit(EXIT_FAILURE);
+  }
+
+  printf("host: %s\n", inet_ntoa(addr.sin_addr));
+
+  res = listen(fd, BACKLOG);
+  while (1) {
+    newsock = accept(fd, (struct sockaddr *) &client_addr, &addrlen);
+
+    if (newsock < 0) {
+      /* code */
+    } else {
+      printf("Connection from %s\n", inet_ntoa(client_addr.sin_addr));
+    }
+    close(newsock);
+  }
+
+  close(fd);
   return 0;
 }
