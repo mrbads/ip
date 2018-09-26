19c801f2e4bbd9ea2fdc9867fed9520830b84bc5
Bas van der Borden <bascasper@gmail.com>
2018-09-26 23:38:15 +0200

    init

---
+++ assignment 2/keyvalue.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+struct table {
+  char key;
+  char value;
+};
+
+char get(char *key) {
+  while (table[i] != NULL) {
+    if (table[i]) {
+      /* code */
+    }
+  }
+}
+
+void put(char *key, char *value) {
+  /* code */
+}
